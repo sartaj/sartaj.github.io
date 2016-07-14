02343c530d5786e277cb3a07a1a1c152fedbc964
Sartaj <sartaj@sartaj.me>
2016-07-13 21:35:53 -0500

    update resume

--- data/index.js
+++ data/index.js
@@ -10,7 +10,7 @@ module.exports = {
   },
   forkUrl: 'https://github.com/sartaj/sartaj.github.io',
   github: 'https://github.com/sartaj',
-  pdf: './generated/SartajChowdhury-resume.pdf',
+  pdf: 'SartajChowdhury-resume.pdf',
   twitter: 'https://twitter.com/sartaj',
   professional,
   personal,
