91b86482bb365d01a7acc2fd982238b3aee2d0c8
Sartaj <sartaj@sartaj.me>
2018-01-16 00:08:53 -0600

    fix bad commit

--- data/index.js
+++ data/index.js
@@ -1,17 +1,18 @@
 import professional from './professional.js'
 import personal from './personal.js'
+import coverImg from '../style/media/cover.jpg'
 
 export default {
   cover: {
-    src: './style/media/cover.jpg',
+    src: coverImg,
     width: '235mm',
     left: '-25mm',
-    top: '-30mm',
+    top: '-30mm'
   },
   forkUrl: 'https://github.com/sartaj/sartaj.github.io',
   github: 'https://github.com/sartaj',
   pdf: 'https://github.com/sartaj/sartaj.github.io/raw/master/SartajChowdhury-Resume.pdf',
   twitter: 'https://twitter.com/sartaj',
   professional,
-  personal,
+  personal
 }
--- src/index.js
+++ src/index.js
@@ -1,11 +1,16 @@
-// const DATA_SOURCE = '../data/index.js'
 import model from '../data/index.js'
 import view from './view'
 import intent from './intent.js'
+import '../style/index.css'
 
 intent()
 
-const CONTAINER_ID = '#container'
+const CONTAINER_ID = 'container'
 
-const $container = document.querySelector(CONTAINER_ID)
+const div = document.createElement('div')
+div.setAttribute('id', CONTAINER_ID)
+document.body.appendChild(div)
+document.title = 'Sartaj'
+
+const $container = document.getElementById(CONTAINER_ID)
 $container.innerHTML = view(model)
